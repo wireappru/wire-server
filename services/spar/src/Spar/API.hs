b0678696fe6408d9f9f95a143e2bedc6f04ee666
Matthias Fischmann <mf@zerobuzz.net>
2018-06-04 09:57:14 +0200

    test suite sceleton.

--- services/spar/src/Spar/API.hs
+++ services/spar/src/Spar/API.hs
@@ -1,11 +1,53 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE TypeFamilies               #-}
+
 module Spar.API where
 
+import Control.Monad.Except
+import GHC.Stack
 import SAML2.WebSSO
+import Servant
 import Spar.Options
+
 
 runServer :: Opts -> IO ()
 runServer = error . show
 
 
-onSuccess :: SPHandler m => UserId -> m Void
+onSuccess :: (HasCallStack, MonadSpar m) => UserId -> m Void
 onSuccess = undefined
+
+
+class MonadSpar m where
+  createUser :: UserId -> m ()
+  forwardBrigLogin :: m Void
+
+
+newtype Spar a = Spar (IO a)
+  deriving (Functor, Applicative, Monad)
+
+instance HasConfig Spar where
+  getConfig = undefined
+
+instance SP Spar where
+  logger = undefined
+  createUUID = undefined
+  getNow = undefined
+
+instance SPStore Spar where
+  storeRequest = undefined
+  checkAgainstRequest = undefined
+  storeAssertion = undefined
+
+instance MonadError ServantErr Spar where
+  throwError = undefined
+  catchError = undefined
+
+instance SPHandler Spar where
+  type NTCTX Spar = ()
+  nt = undefined
+
+instance MonadSpar Spar where
+  createUser = undefined
+  forwardBrigLogin = undefined
--- services/spar/test/Test/Spar/APISpec.hs
+++ services/spar/test/Test/Spar/APISpec.hs
@@ -1,20 +1,19 @@
 module Test.Spar.APISpec where
 
-import Test.Hspec
 import Spar.API ()
+import Test.Hspec
 
 
 spec :: Spec
 spec = do
   describe "onSuccess" $ do
     context "new user" $ do
-      it "stores user on cassandra" $ do
-        False `shouldBe` True
-
-      -- ...
+      it "calls createUser" $ do
+        pending
 
     context "known user" $ do
-      it "does not change cassandra state" $ do
-        False `shouldBe` True
+      it "does not call createUser" $ do
+        pending
 
-    -- ...
+    it "calls forwardBrigLogin" $ do
+      pending
